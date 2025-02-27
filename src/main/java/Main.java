0b436a0d010d9b83c1c92b63e202c1d9471fd21d
Paolo Pietrelli <pablos3339@gmail.com>
2025-02-27 16:48:40 +0100

    print person age

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,6 @@ import utils.Logger;
 public class Main {
     public static void main(final String[] args){
         Person person = new Person("Gianluca", 29);
-        Logger.logPrintString("Hello " + person);
+        Logger.logPrintString("Hello " + person + " " + person.getAge());
     }
 }
